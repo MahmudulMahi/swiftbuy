7ad5f7059941d19777da37b8ab70ad23a862de5a
mahmudul <mahmudul35-218@diu.edu.bd>
2026-02-23 23:14:36 +0600

    create overlay

--- components/CartSidebar.tsx
+++ components/CartSidebar.tsx
@@ -18,7 +18,13 @@ export default function CartSidebar({ isOpen, onClose }: CartSidebarProps) {
 
   return (
     <>
-
+      {/* Overlay */}
+      {isOpen && (
+        <div
+          className="fixed inset-0 bg-black bg-opacity-50 z-40 transition-opacity"
+          onClick={onClose}
+        />
+      )}
 
       {/* Sidebar */}
       <div
